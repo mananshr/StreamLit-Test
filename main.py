37d4d5e103753bdd99498c6fa285420f9a5974bf
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-07-31 13:24:35 +0530

    To deployment on StreamLit

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import streamlit as st
 import yfinance as yf
+import datetime
 
-st.write("""
-    # Stock Price Analysis""")
 
-# get data for Apple
+st.write(""" 
+            # Stock Price Analyser """)
 
-symbol = "TSLA"
+symbol = st.selectbox(
+    'Which Stock Symbol would you want to analyse?',
+    ('AAPL', 'GOOG', 'TSLA', 'MSFT', 'NFLX'))
 
-# symbol = st.selectbox("Select symbol", "AAPL", "GOOG", "TSLA", "NFLX")
+col1, col2 = st.columns(2)
+
+with col1:
+    start_date = st.date_input("Please enter start date", datetime.date(2019, 7, 6))
+
+with col2:
+    end_date = st.date_input("Please enter end date", datetime.date(2019, 7, 10))
 
 ticker_data = yf.Ticker(symbol)
-ticker_df = ticker_data.history(period="1d", start="2019-01-01", end="2022-12-31")
-# ticker_df = ticker_data.history(period="1d")
+ticker_df = ticker_data.history(period="1d", start= start_date, end=end_date)
 
-st.write("""
-    ### stock price data""")
+st.write(f"""
+    ### {symbol}'s Stock price data """)
 
 st.dataframe(ticker_df)
+
+st.write(f"""
+    ### {symbol}'s Closing Price Chart price data""")
+
 st.line_chart(ticker_df["Close"])
+
+st.write(f"""
+    ### {symbol}'s Volume Chart""")
+
+st.line_chart(ticker_df["Volume"])
+
